0e98849dfcc70d4bef582713a9a5bd67960e2b5d
Drekorig <drekorig@gmail.com>
2020-04-29 18:06:29 +0200

    release 1.1

--- TrackerOOT/Form1.Designer.cs
+++ TrackerOOT/Form1.Designer.cs
@@ -653,7 +653,7 @@ namespace TrackerOOT
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "Form1";
             this.ShowIcon = false;
-            this.Text = "OOTR : Gossip Stones Tracker";
+            this.Text = "OOTR : Gossip Stones Tracker v1.1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
